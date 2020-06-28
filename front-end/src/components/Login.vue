57d86d4a355d5c1dbe330fd8d30f42ffe554ffbb
Larissa Pissurno <larissapissurno@gmail.com>
2020-06-27 23:18:38 -0300

    Login page

---
+++ front-end/src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="login-box">
+    <h1>login</h1>
+
+    <md-field :class="messageClass">
+      <label>E-mail</label>
+      <md-input type="email" v-model="email" required></md-input>
+      <span class="md-error">There is an error</span>
+    </md-field>
+
+    <md-field :class="messageClass">
+      <label>Password</label>
+      <md-input type="password" v-model="password" required></md-input>
+      <span class="md-error">There is an error</span>
+    </md-field>
+
+    <md-button class="md-raised md-primary md-layout-item md-size-100" md-size-100
+      >Entrar</md-button
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  @Prop() private msg!: string;
+
+  hasMessages = false;
+
+  email = '';
+
+  password = '';
+
+  get messageClass(): string {
+    return this.hasMessages ? 'md-invalid' : '';
+  }
+}
+</script>
+<style scoped lang="scss">
+div.login-box {
+  background-color: #ffff;
+  padding: 26px;
+  min-width: 500px;
+  border-radius: 20px;
+  -webkit-box-shadow: 0px 0px 50px 0px rgba(178, 178, 178, 0.6);
+  -moz-box-shadow: 0px 0px 50px 0px rgba(178, 178, 178, 0.6);
+  box-shadow: 0px 0px 50px 0px rgba(178, 178, 178, 0.6);
+  button {
+    margin-top: 20px;
+  }
+}
+</style>
---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Login,
+  },
+};
+</script>
+<style lang="scss">
+div.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: url('../assets/Business_PNG.png') no-repeat;
+  background-position: 95% 85%;
+  background-size: 500px;
+}
+</style>
